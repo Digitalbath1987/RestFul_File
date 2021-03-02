8d2b42f172c2ff28e156b6bce3f429256d206219
Digitalbath1987 <mario.rosales02@inacapmail.cl>
2021-03-02 19:16:08 -0300

    Add files via upload

---
+++ SRV.RESTFulAlo/CompressAttribute.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRV.RESTFulAlo
+{
+    public class CompressAttribute : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var response = HttpContext.Current.Response;
+
+
+            var acceptEncodingHeader = HttpContext.Current.Request.Headers["Accept-Encoding"];
+            if (!string.IsNullOrEmpty(acceptEncodingHeader) &&
+                ((acceptEncodingHeader.Contains("gzip") || acceptEncodingHeader.Contains("deflate"))))
+            {
+                response.Headers.Remove("Content-Encoding");
+                if (acceptEncodingHeader.Contains("gzip"))
+                {
+                    response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+                    response.AppendHeader("Content-Encoding", "gzip");
+                }
+                else
+                {
+                    response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
+                    response.AppendHeader("Content-Encoding", "deflate");
+                }
+            }
+
+           
+        }
+
+        /// <summary>
+        /// EVENTO AL FINALIZAR METODO
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+
+            try
+            {
+
+
+                
+                //===========================================================
+                // RUTAS
+                //===========================================================
+                string FolderUpload = System.Web.HttpContext.Current.Server.MapPath("~/FileUpload/");
+                string FolderDownload = System.Web.HttpContext.Current.Server.MapPath("~/FileDownload/");
+
+
+
+                //=========================================================
+                // FECHA ACTUAL
+                //=========================================================
+                DateTime FECHA_ACTUAL = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+
+                //=========================================================
+                // LECTURA DE CARPETA UPLOAD
+                //=========================================================
+                DirectoryInfo DirUpload = new DirectoryInfo(FolderUpload);
+                foreach (var FileItem in DirUpload.GetFiles("*.*"))
+                {
+
+                    FileInfo FileToken = new FileInfo(FileItem.FullName);
+                    DateTime FECHA_FILE = Convert.ToDateTime(FileToken.CreationTime.ToShortDateString());
+
+
+                    //=====================================================
+                    // DIAS
+                    //=====================================================
+                    int Dias = ((TimeSpan)(FECHA_ACTUAL - FECHA_FILE)).Days;
+
+
+                    if (Dias == 1)
+                    {
+
+
+                        //=================================================
+                        // ELIMINAR ARCHIVO
+                        //=================================================
+                        File.Delete(FileItem.FullName);
+
+
+                    }
+
+
+
+                }
+
+                //=========================================================
+                // LECTURA DE CARPETA DOWNLOAD
+                //=========================================================
+                DirectoryInfo DirDownload = new DirectoryInfo(FolderDownload);
+                foreach (var FileItem in DirDownload.GetFiles("*.*"))
+                {
+
+                    FileInfo FileToken = new FileInfo(FileItem.FullName);
+                    DateTime FECHA_FILE = Convert.ToDateTime(FileToken.CreationTime.ToShortDateString());
+
+
+                    //=====================================================
+                    // DIAS
+                    //=====================================================
+                    int Dias = ((TimeSpan)(FECHA_ACTUAL - FECHA_FILE)).Days;
+
+
+                    if (Dias > 2)
+                    {
+
+
+                        //=================================================
+                        // ELIMINAR ARCHIVO
+                        //=================================================
+                        File.Delete(FileItem.FullName);
+
+
+                    }
+
+
+
+                }
+
+
+            }
+            catch { }
+
+
+        }
+
+
+    }
+}
---
+++ SRV.RESTFulAlo/FuncionesGenerales.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using SRV.Entidades;
+
+namespace SRV.RESTFulAlo
+{
+    public static class FuncionesGenerales
+    {
+
+        /// <summary>
+        /// CONFIGURACIÓN DE ERRORES 
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static ErroresException CONFIGURA_ERRORES(Exception ex)
+        {
+
+            ErroresException Entidad = new ErroresException();
+
+
+            try
+            {
+
+
+
+                MethodBase MetodoGatillante = ex.TargetSite;
+                Type Class = MetodoGatillante.ReflectedType;
+
+
+                //===========================================================
+                // OPCIONES DE METODO GATILLANTE                           ==
+                //===========================================================
+                Entidad.NombreMetodo = MetodoGatillante.Name;
+                Entidad.Clase = Class.FullName;
+                Entidad.NameSpace = Class.Namespace;
+
+                string Mensaje = ex.Message;
+                Mensaje = Mensaje.Replace("\"", " ");
+                Mensaje = Mensaje.Replace("'", " ");
+
+
+                Entidad.Mensaje = Mensaje;
+
+
+                //===========================================================
+                // METODO EN CUAL FUE LA CAIDA                             ==
+                //===========================================================
+                List<Secuencia> Lista = new List<Secuencia>();
+                StackTrace st = new StackTrace(ex, true);
+                for (int i = st.FrameCount - 1; i >= 0; i--)
+                {
+
+                    StackFrame sf = st.GetFrame(i);
+                    System.Reflection.MethodBase method = sf.GetMethod();
+                    Lista.Add(new Secuencia { Item = method.Name });
+                }
+                Entidad.Eventos = Lista;
+
+
+
+
+
+                return Entidad;
+
+            }
+            catch
+            {
+                return Entidad;
+
+            }
+
+        }
+
+
+    }
+}
