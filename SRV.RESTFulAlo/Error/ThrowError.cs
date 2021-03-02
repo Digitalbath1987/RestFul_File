043c96404c053df7a22ed5a3aa6072084d7fdc72
Digitalbath1987 <mario.rosales02@inacapmail.cl>
2021-03-02 19:21:05 -0300

    Add files via upload

---
+++ SRV.RESTFulAlo/Error/ThrowError.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using SRV.Entidades;
+
+namespace SRV.RESTFulAlo.Error
+{
+    public static class ThrowError
+    {
+
+        
+
+
+        /// <summary>
+        /// CREACIÓN DE ENTIDADES
+        /// </summary>
+        /// <param name="Entidad"></param>
+        /// <param name="Root"></param>
+        /// <returns></returns>
+        private static XmlDocument GetEntityXml(ErroresException Entidad, String Root)
+        {
+
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            try
+            {
+
+
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("", "");
+                XmlAttributeOverrides overrides = new XmlAttributeOverrides();
+                XmlAttributes attr = new XmlAttributes();
+                attr.XmlRoot = new XmlRootAttribute(Root);
+                overrides.Add(typeof(ErroresException), attr);
+
+
+
+                XPathNavigator nav = xmlDoc.CreateNavigator();
+                using (XmlWriter writer = nav.AppendChild())
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(ErroresException), overrides);
+                    ser.Serialize(writer, Entidad, ns);
+                }
+
+
+
+
+                return xmlDoc;
+            }
+            catch
+            {
+
+                return xmlDoc;
+            }
+        }
+
+
+
+        /*----------------------------------------------------------------------*/
+        /* MENSAJES THROW                                                       */
+        /*----------------------------------------------------------------------*/
+        public static String MensajeThrow(ErroresException Entidad)
+        {
+
+            String Mensaje = "";
+
+            try
+            {
+
+
+                //===========================================================
+                // DECLARACION DE VARIABLES                                ==
+                //===========================================================
+                XmlDocument Documento = new XmlDocument();
+
+
+
+                //===========================================================
+                // CREACIÓN DE XML                                         ==
+                //===========================================================
+                Documento = GetEntityXml(Entidad, "Detalles");
+
+
+                //===========================================================
+                // RUTA XSLT TRANSFORMADOR                                 ==
+                //===========================================================
+                string RUTA_XSLT = HttpContext.Current.Server.MapPath("~/Error/ThrowError.xslt");
+
+
+
+                //===========================================================
+                // TRANSFORMA XML                                          ==
+                //===========================================================
+                XPathDocument doc = new XPathDocument(new StringReader(Documento.InnerXml.ToString()));
+                XslCompiledTransform xslt = new XslCompiledTransform();
+                xslt.Load(RUTA_XSLT);
+                StringWriter sw = new StringWriter();
+                xslt.Transform(doc, null, sw);
+
+
+                return sw.ToString();
+
+
+            }
+            catch
+            {
+                return Mensaje;
+            }
+
+        }
+
+
+
+    }
+
+}
---
+++ SRV.RESTFulAlo/Helpers/Controles.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRV.RESTFulAlo.Helpers
+{
+    public static class Controles
+    {
+
+
+        /// <summary>
+        /// RESOLVER URL DINAMICA
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="UrlHelper"></param>
+        /// <returns></returns>
+        private static string ResolveURL(string url, UrlHelper UrlHelper)
+        {
+
+            string URL = UrlHelper.Content(url);
+            return URL;
+        }
+
+        /// <summary>
+        /// RESOLVER URL DINAMICA CSS
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="cssURL"></param>
+        /// <returns></returns>
+        public static MvcHtmlString CSSLink(this HtmlHelper helper, string cssURL)
+        {
+            var UrlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            return new MvcHtmlString(string.Format("<link href='{0}' rel='stylesheet' type='text/css'/>",
+                                     ResolveURL(cssURL, UrlHelper)));
+        }
+
+        /// <summary>
+        /// RESOLVER URL DINAMICA JS
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="jsURL"></param>
+        /// <returns></returns>
+        public static MvcHtmlString JSLink(this HtmlHelper helper, string jsURL)
+        {
+
+            var UrlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            return new MvcHtmlString(string.Format("<script src='{0}' type='text/javascript'></script>",
+                                     ResolveURL(jsURL, UrlHelper)));
+        }
+
+
+        
+        /// <summary>
+        /// RESOLVER URL
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="URL"></param>
+        /// <returns></returns>
+        public static MvcHtmlString Href(this HtmlHelper helper, string URL)
+        {
+            var UrlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            return new MvcHtmlString(ResolveURL(URL, UrlHelper));
+        }
+
+    }
+}
